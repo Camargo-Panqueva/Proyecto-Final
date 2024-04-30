1d86c4d2272f04c9ac9c8e61a8d6e24bc7001ed6
t-hash06 <tomasfelipe.p@gmail.com>
2024-04-30 09:55:52 -0500

    [CREATE] Documentation for Window package

--- src/view/window/Window.java
+++ src/view/window/Window.java
@@ -3,12 +3,32 @@ package view.window;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Represents a window object that can be displayed on the screen.
+ * <p>
+ * This class represents a window object that can be displayed on the screen.
+ * It provides a structure for creating a window with a canvas.
+ * The window can be displayed on the screen and resized.
+ * The window can also have a title that is displayed in the title bar.
+ * </p>
+ */
 public final class Window {
 
     private final JFrame frame;
     private final Canvas canvas;
     private final String title;
 
+    /**
+     * Creates a new Window with the given size and title.
+     * <p>
+     * This constructor creates a new Window with the given size and title.
+     * The window is created with a canvas of the given size.
+     * The window is created with a title that is displayed in the title bar.
+     * </p>
+     *
+     * @param size  the size of the window.
+     * @param title the title of the window.
+     */
     public Window(final int size, final String title) {
         this.title = title;
 
@@ -19,6 +39,15 @@ public final class Window {
         this.setupJFrame(title);
     }
 
+    /**
+     * Sets up the JFrame with the given title.
+     * <p>
+     * This method sets up the JFrame with the given title.
+     * It adds the canvas to the JFrame and sets the JFrame properties.
+     * </p>
+     *
+     * @param title the title of the JFrame.
+     */
     private void setupJFrame(final String title) {
         this.frame.add(this.canvas);
 
@@ -31,32 +60,64 @@ public final class Window {
         this.frame.setLocationRelativeTo(null);
     }
 
+    /**
+     * Sets up the canvas with the given size.
+     * <p>
+     * This method sets up the canvas with the given size.
+     * </p>
+     *
+     * @param size the size of the canvas.
+     */
     private void setupCanvas(int size) {
         this.canvas.setPreferredSize(new Dimension(size, size));
     }
 
+    /**
+     * Makes the window visible on the screen.
+     */
     public void makeVisible() {
         this.frame.setVisible(true);
     }
 
+    /**
+     * Gets the title of the window.
+     *
+     * @return the title of the window.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Gets the size of the canvas.
+     * @return the size of the canvas.
+     */
     public int getCanvasSize() {
         return this.canvas.getWidth();
     }
 
+    /**
+     * Sets the size of the canvas.
+     * @param size the size of the canvas.
+     */
     public void setCanvasSize(int size) {
         this.canvas.setPreferredSize(new Dimension(size, size));
         this.frame.pack();
         this.frame.setLocationRelativeTo(null);
     }
 
+    /**
+     * Gets the JFrame of the window.
+     * @return the JFrame of the window.
+     */
     public JFrame getFrame() {
         return frame;
     }
 
+    /**
+     * Gets the canvas of the window.
+     * @return the canvas of the window.
+     */
     public Canvas getCanvas() {
         return canvas;
     }
