f801747ea797d73fd8f1873a7953f6bfd0ae2ab9
t-hash06 <tomasfelipe.p@gmail.com>
2024-04-30 10:08:00 -0500

    [CREATE] Documentation for Util package

--- src/util/VoidFunction.java
+++ src/util/VoidFunction.java
@@ -1,5 +1,11 @@
 package util;
 
+/**
+ * Represents a function that does not return a value.
+ * <p>
+ * This interface represents a function that does not return a value.
+ * </p>
+ */
 @FunctionalInterface
 public interface VoidFunction {
     void run();
