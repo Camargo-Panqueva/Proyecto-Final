c296ef1b9dc8001ddfd3813872a8290b9786d838
CamargoT <juanpablocamargo91@gmail.com>
2024-05-11 23:45:03 -0500

    [CREATE] Test Suite

---
+++ test/TestQuoridor.java
@@ -0,0 +1,154 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class Quopripoobv1.
+ *
+ * @author  POOB
+ * @version 2024-1
+ */
+public class TestQuoridor
+{
+    /**
+     * Default constructor for test class Qupripoobv1
+     */
+    public TestQuoridor()
+    {
+    }
+
+    @Test
+    public void shouldCreateBoardsOfDifferentSizes(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldAssignBarriersToPlayers(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldMoveOrthogonallyAPawn(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldMoveDiagonallyAPawn(){
+        fail();
+    }
+
+    @Test
+    public void shouldPlaceANormalBarrier(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldMoveAPawnOverAPawn(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldNotMoveAPawnOverANonAlliedBarrier(){
+        fail();
+    }
+
+    @Test
+    public void shouldMoveAPawnOverAnAlliedBarrier(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldKnowWhenSomeoneWonTheGame(){
+        fail();
+    }
+
+    @Test
+    public void shouldKnowTheBarriersLeftForEachPlayer(){
+        fail();
+    }
+
+    @Test
+    public void shouldNotBlockThePassageOfAPlayer(){
+        fail();
+    }
+
+    @Test
+    public void shouldMeetNormalBarrierConditions(){
+        fail();
+    }
+
+    @Test
+    public void shouldMeetTemporalBarrierConditions(){
+        fail();
+    }
+
+    @Test
+    public void shouldMeetLongBarrierConditions(){
+        fail();
+    }
+
+    @Test
+    public void shouldMeetAlliedBarrierConditions(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldNotCreateABoardIfItsNotPossible(){
+        fail();
+    }
+
+
+
+    @Test
+    public void shouldNotMoveOrthogonallyAPawnIfItsNotPossible(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldNotMoveDiagonallyAPawnIfItsNotPossible(){
+        fail();
+    }
+
+    @Test
+    public void shouldNotPlaceANormalBarrierIfItsNotPossible(){
+        fail();
+    }
+
+
+    @Test
+    public void shouldNotMoveAPawnOverAPawnIfItsNotPossible(){
+        fail();
+    }
+
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
