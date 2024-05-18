b91cda8f96be192c6a88f4e3e4147ae7c5124e02
CamargoT <juanpablocamargo91@gmail.com>
2024-05-18 11:32:11 -0500

    [UPDATE] Cells Implements ActionableElements

--- src/controller/cells/Cell.java
+++ src/controller/cells/Cell.java
@@ -1,13 +1,11 @@
 package controller.cells;
 
+import controller.ActionableElement;
 import model.GameModel;
 
-public abstract class Cell {
+public abstract class Cell implements ActionableElement {
 
     public Cell() {
     }
-
-    public abstract void action(GameModel model);
-
 
 }
