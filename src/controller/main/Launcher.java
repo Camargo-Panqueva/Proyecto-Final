b08e8b82b9ed3ce4f868b7e2c856c06c5067d384
t-hash06 <tomasfelipe.p@gmail.com>
2024-05-26 22:53:36 -0500

    [UPDATE] Removed test code on Launcher class

--- src/controller/main/Launcher.java
+++ src/controller/main/Launcher.java
@@ -2,7 +2,6 @@ package controller.main;
 
 import controller.GameController;
 import model.GameModel;
-import util.Timeout;
 import view.GameView;
 
 public final class Launcher {
@@ -12,12 +11,6 @@ public final class Launcher {
         GameModel gameModel = new GameModel();
         GameController gameController = new GameController(gameModel);
         GameView gameView = new GameView(gameController);
-
-        Timeout timeOut = new Timeout(()->{
-            System.out.println("TimeOut");
-        }, 3000);
-
-        timeOut.start();
 
         gameView.start();
     }
